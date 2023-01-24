e4dbb651ca05bc4c71b1b5071100d974f4d38156
enoviell <123171897+enoviell@users.noreply.github.com>
2023-01-24 13:06:50 +0100

    Add files via upload

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: enoviell <enoviell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/23 17:07:55 by enoviell          #+#    #+#             */
-/*   Updated: 2023/01/23 17:56:45 by enoviell         ###   ########.fr       */
+/*   Created: 2023/01/24 13:01:34 by enoviell          #+#    #+#             */
+/*   Updated: 2023/01/24 13:05:23 by enoviell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_list	*ft_lstnew(void *content)
 	new_node = malloc(sizeof(t_list));
 	if (!new_node)
 		return (NULL);
-	new_node -> content;
-	new_node -> NULL;
+	new_node -> content = content;
+	new_node -> next = NULL;
 	return (new_node);
 }
